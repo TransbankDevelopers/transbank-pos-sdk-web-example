1faba1cd49c1154f5894c6ec0167f1b06f548050
DarkFrost <m.astudillo.toledo@gmail.com>
2021-05-26 14:38:05 -0400

    Add poll component

---
+++ src/components/Poll.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2 class="text-xl">Poll</h2>
+    <button class="bg-green-600 hover:bg-green-700 px-5 py-2 shadow rounded text-white" @click="poll">
+      <span v-if="!waiting">Realizar Poll</span>
+      <span v-if="waiting">Haciendo Poll...</span>
+    </button>
+    <pre>{{ response }}</pre>
+  </div>
+</template>
+<script>
+    import POS from "transbank-pos-sdk-web"
+
+    export default {
+        data() {
+            return {
+                waiting: false,
+                response: null,
+            }
+        },
+        methods: {
+            poll() {
+                if (this.waiting) return;
+
+                this.waiting = true;
+                this.response = null;
+                POS.poll().then((response) => {
+                    this.response = response
+                }).finally(() => {
+                    this.waiting = false;
+                })
+
+            },
+        },
+    }
+</script>

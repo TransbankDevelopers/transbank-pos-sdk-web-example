efbfda8c7e1da32707e718f815be742f555a0b59
DarkFrost <m.astudillo.toledo@gmail.com>
2021-05-27 20:46:16 -0400

    Add operation result component

---
+++ src/components/OperationResult.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="resultData !== null">
+    <h2 class="text-2xl">Respuesta: </h2>
+    <vue-json-pretty :data="resultData" :showDoubleQuotes="false" :showLength="true"> </vue-json-pretty>
+  </div>
+</template>
+
+<script>
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+
+export default {
+  components: {
+    VueJsonPretty,
+  },
+  props: ["resultData"],
+};
+</script>
